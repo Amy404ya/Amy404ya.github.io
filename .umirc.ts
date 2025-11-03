09e3afc719412181afdc427bb1b855a4660ee283
卢萍 <lupin@landhightech.com>
2025-11-03 13:36:18 +0800

    feat

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from "umi";
 
+const repositoryName = 'Amy404ya.github.io';
+
+const isUserPage = repositoryName.includes('.github.io');
+const publicPath = isUserPage ? '/' : `/${repositoryName}/`;
+const base = isUserPage ? '/' : `/${repositoryName}/`;
+
 export default defineConfig({
+  title: 'SecretHut',
+  publicPath,
+  base,
+  history: {
+    type: 'hash',
+  },
   routes: [
     { path: "/", component: "Home" },
     { path: "/test", component: "Test" },
   ],
   npmClient: 'pnpm',
-  base: '/Amy404ya.github.io/',
-  publicPath: '/Amy404ya.github.io/',
-  history: {
-    type: 'browser',
-  },
+  favicons: ['/yay.jpg'],
+  metas: [
+    {
+      name: 'description',
+      content: 'SecretHut',
+    },
+  ],
 });
