c25ca18b48e8b985a01061344edbc7d9676924fb
卢萍 <lupin@landhightech.com>
2025-11-03 11:35:06 +0800

    feat:新增配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,4 +6,9 @@ export default defineConfig({
     { path: "/test", component: "Test" },
   ],
   npmClient: 'pnpm',
+  base: '/Amy404ya.github.io/',
+  publicPath: '/Amy404ya.github.io/',
+  history: {
+    type: 'browser',
+  },
 });
