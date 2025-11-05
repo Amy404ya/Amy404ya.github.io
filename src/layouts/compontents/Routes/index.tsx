af6c96bd6fb7793d81b9eab4a8a6bf0668cdbadd
卢萍 <lupin@landhightech.com>
2025-11-05 18:08:19 +0800

    [dev]:补充

--- src/layouts/compontents/Routes/index.tsx
+++ src/layouts/compontents/Routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useAppData } from 'umi'
+import styles from './index.less'
 
 export default () => {
   const routes = [
@@ -8,9 +9,9 @@ export default () => {
   ]
 
   return (
-    <div>
+    <div className={styles.routesWrap}>
       {routes.map((route: any) => (
-        <div key={route.path as string}>
+        <div key={route.path as string} className={styles.linkItem}>
           <Link to={route.path as string}>{route.name}</Link>
         </div>
       ))}
